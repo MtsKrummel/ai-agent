b70455c7e26119964dad8cdcf9ea1ba59ebdae56
Matias Krummel <matikrummel.l.2004@gmail.com>
2025-11-16 20:47:14 -0300

    feat(meetings): Implement meetings procedures and schema

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const page = () => {
+const Page = () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(
+    trpc.meetings.getMany.queryOptions({})
+  )
   return (
-    <div>Welcome to meetings!</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
-export default page
+export default Page;
